cc6a73dcfa4a87cf24ea8bd70b856459bae61cb5
LilunCheng <lilun_cheng@yahoo.com>
2022-01-24 22:56:03 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -4,7 +4,7 @@ import os
 def train():
   print("Tracking URI is " + str(os.environ['MLFLOW_ENDPOINT']))
   mlflow.set_tracking_uri(os.environ['MLFLOW_ENDPOINT'])
-  expid = mlflow.create_experiment('lilun_training_experiment')
+  expid = mlflow.create_experiment('lilun_training_experiment_01')
   with mlflow.start_run(experiment_id=expid) as run:
     mlflow.log_param("batch_size", 300)
     mlflow.log_metric("loss", 0.001)
