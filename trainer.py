ffebafc93e2aa8e0e63b5f89e5d018989d034611
LilunCheng <lilun_cheng@yahoo.com>
2022-01-24 21:16:34 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,13 +1,12 @@
+import mlflow
+from mlflow import log_metric, log_param, log_artifacts
+import os
 def train():
-  import mlflow
-  import sys,os,os.path  
-  from mlflow import log_metric, log_param, log_artifacts
-  
   mlflow.set_tracking_uri(os.environ['MLFLOW_ENDPOINT'])
   with mlflow.start_run(run_name="Lilun_Model_run") as run:
     mlflow.log_param("batch_size", 300)
     mlflow.log_metric("loss", 0.001)
-    
+
 def trainer():
   print("Start training ......")
   train()
