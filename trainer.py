47ed911ddc3280927a161cbe2551eef27be24b38
LilunCheng <lilun_cheng@yahoo.com>
2022-01-25 21:57:28 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -2,15 +2,12 @@ import mlflow
 from mlflow import log_metric, log_param, log_artifacts
 import os
 def train():
-  print("Tracking URI is " + str(os.environ['MLFLOW_ENDPOINT']))
-  mlflow.set_tracking_uri(os.environ['MLFLOW_ENDPOINT'])
-  #expid = mlflow.create_experiment('lilun_training_experiment_02')
-  print("Returned tracking is "+mlflow.tracking.get_tracking_uri())
-  with mlflow.start_run(run_name="Lilun Experiment") as run:
-  #with mlflow.start_run(experiment_id=expid) as run:
-    print("Debug ...")
-    mlflow.log_param("batch_size", 300)
-    mlflow.log_metric("loss", 0.001)
+	with mlflow.start_run(run_name="Dummy") as run:
+		print("Start MLFlow Tracking ...")
+
+	with mlflow.start_run(run_name="Dummy") as run:
+		mlflow.log_param("batch_size", 300)
+		mlflow.log_metric("loss", 0.001)
 
 def trainer():
   print("Start training ......")
